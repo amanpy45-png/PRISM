49c8e68d8ceb99dcf1a15f910e6a844be35748d7
Aman Negi <263108657+amanpy45-png@users.noreply.github.com>
2026-09-23 22:47:05 +0530

    Update loader.py
    Removed unnecessary comments from loader.py.

--- src/loader.py
+++ src/loader.py
@@ -1,15 +1,3 @@
-# PDF
-#    │
-#    ▼
-# PyPDFLoader
-#    │
-#    ▼
-# List[Document]
-#    │
-#    ├── page_content  ← Actual text
-#    └── metadata      ← Source, page number, etc.
-
-
 from langchain_community.document_loaders import PyPDFLoader
 
 def load_pdf(pdf_path):
