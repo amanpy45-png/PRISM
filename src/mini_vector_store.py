fab6ccaacddf94b1528dbf9cce04fe9e8b73c9f9
Aman Negi <263108657+amanpy45-png@users.noreply.github.com>
2026-09-25 22:02:41 +0530

    Update mini_vector_store.py

--- src/mini_vector_store.py
+++ src/mini_vector_store.py
@@ -15,13 +15,10 @@ chunks = [item[1] for item in vector_store]
 
 def retrieve(query, k=3):
     query_embedding = model.encode(query)
-
     query_embedding = query_embedding.reshape(1, -1)
 
     similarities = cosine_similarity(query_embedding, document_embeddings)
-
     scores = similarities[0]
-
     top_k = np.argsort(scores)[::-1][:k]
 
     return [vector_store[i][1] for i in top_k]
